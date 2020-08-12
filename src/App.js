3af8900c138f884e4fb207db88acb20ce7e7ef41
Yuri Salgado <yuri.salgado@simplustec.com.br>
2020-08-12 20:09:12 -0300

    boot conversor de moedas ProgramadorBR

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import './App.css';
+
+import Conversor from './components/Conversor';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      	<div className="linha">
+	        <Conversor moedaA="USD" moedaB="BRL"/>
+	        <Conversor moedaA="BRL" moedaB="USD"/>
+      	</div>
+      	<div className="linha">
+	        <Conversor moedaA="CAD" moedaB="BRL"/>
+	        <Conversor moedaA="BRL" moedaB="CAD"/>
+      	</div>
+      	<div className="linha">
+	        <Conversor moedaA="EUR" moedaB="BRL"/>
+	        <Conversor moedaA="BRL" moedaB="EUR"/>
+      	</div>
+      </div>
+    );
+  }
+}
+
+export default App;
